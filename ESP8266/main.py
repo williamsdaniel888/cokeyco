aa4775117ebaceab9109f2792000614a30c45936
Piersdb <piersdb@users.noreply.github.com>
2018-02-15 19:35:57 +0000

    Revert "Updated MQTT details"
    This reverts commit 43c96bd30baf8eeae409e7f398752287930aa578.

--- ESP8266/main.py
+++ ESP8266/main.py
@@ -16,15 +16,21 @@ movingAverageSampleSize=16
 # ap_if.active(False)
 sta_if= network.WLAN(network.STA_IF)
 sta_if.active(True)
-sta_if.connect('pomelo', 'hellohelloX2')
+sta_if.connect('EEERover', 'exhibition')
 if sta_if.isconnected():
     print("Connected to Wifi")
 
 #MQTT
 counter = 0
-client = MQTTClient("picly", "192.168.137.145")
+client = MQTTClient("test", "192.168.0.10")
 time.sleep(5)
 client.connect()
+# while True:
+#     payload = ujson.dumps({'YOLO':'{0} Is the message'.format(counter)})
+#     client.publish("home",bytes(payload,'utf-8'))
+#     counter +=1
+#     time.sleep(5)
+# #to observe channel, type the following into Powershell: mosquitto_sub -t "home" -h 192.168.0.10
 
 #create the i2cport
 i2cport = I2C(scl=Pin(5), sda=Pin(4), freq=100000)
@@ -65,16 +71,24 @@ def moving_average_filter(dataX,dataY,dataZ):
 
 while True:
     #send the command for single-measurement mode
+    # TODO: Do we need to do this for each reading?
     i2cport.writeto(0x1E, bytearray([0x02, 0x01]))
     #Wait 6ms
+    #TODO: Why do we need to do this? Synchronisation?
     time.sleep_ms(7)
 
+
     data = i2cport.readfrom(0x1E, 0x06)
+
 
     #convert the six bytes to three ints
     dataX = convert_mag_readings_to_int(bytearray([data[0],data[1]]))   
     dataZ = convert_mag_readings_to_int(bytearray([data[2],data[3]]))
     dataY = convert_mag_readings_to_int(bytearray([data[4],data[5]]))
+
+
+
+    
 
     #Moving Average Filter
     average = moving_average_filter(dataX,dataY,dataZ)
@@ -83,6 +97,25 @@ while True:
     dataZ  = average[2]
     #print("Average is", average)
 
+
     sjson = "{{'dataX':{0},'dataY':{1},'dataZ':{2}}}".format(dataX,dataY,dataZ)
-    client.publish("home",bytes(sjson,'utf-8'))
-    time.sleep(3)
+    client.publish("magReadings",bytes(sjson,'utf-8'))
+
+    if Program_Counter %16==0:
+        print(sjson)
+
+    if Activation_Hold_Off_Counter>200:
+        ####TODO: DO DSP HERE
+        if dataX >800 and dataY >800:
+            #When in Quadrant x
+            client.publish("musicControl",bytes("next",'utf-8'))
+            print("Next Activated")
+            Activation_Hold_Off_Counter = 0
+        elif dataX<-800 and dataY <-800:
+            client.publish("musicControl",bytes("previous",'utf-8'))
+            print("Previous Activated")
+            Activation_Hold_Off_Counter = 0
+
+    Activation_Hold_Off_Counter+=1
+    Program_Counter+=1
+    #time.sleep(1)
